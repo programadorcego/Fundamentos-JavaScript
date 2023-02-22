c34a9c46eb4b6c70a352dc1f40fc7e2645e6c319
Programador Cego <programadorcego@gmail.com>
2023-02-22 10:13:17 -0300

    Aula 06: Tipos de Dados

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
- //Constante que armazena o valor de PI
-const pi = 3.14;
-console.log(pi);
+/*
+string
+let a = "oi";
+let a = 'oi';
+let a = `oi`;
+**/
+
+/*
+Number
+let a = 1;
+let a = -10;
+let a = 3.5;
+/*
+
+/*
+Boolean
+let a = true;
+let a = false;
+*/
+
+/*
+NULL
+let a = null;
+*/
+
+/*
+Undefined
+let a = undefined;
+*/
+
+/*
+Object
+let a = [];
+let a = {};
+*/
+
+console.log(typeof a);
