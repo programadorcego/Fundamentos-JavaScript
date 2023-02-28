737e49c702d61afb13de133fa0b04dcb699786ab
Programador Cego <programadorcego@gmail.com>
2023-02-28 10:17:18 -0300

    Aula 12: Concatenação

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-console.log((1 + 5) * 3);
-
-// Ordem de Precedência
-// Operadores Aritméticos
-
-// 1. Potenciação
-
-// 2. Multiplicação, divisão e módulo
-// 3. Adição e subtração
-
-// Operadores de Comparação
-
-// 4. <, >, <=, >=
-// 5. ==, !=, ===, !==
-
-// Operadores Lógicos
-
-// 6. &&
-// 7. ||
+let name = "Willian";
+let lastname = "Pereira";
+//console.log(name + lastname);
+console.log(`Meu nome é ${name} ${lastname}`);
