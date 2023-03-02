538ed5f194f149a6e0823d4b90d8a7653912d87b
Programador Cego <programadorcego@gmail.com>
2023-03-02 18:44:43 -0300

    Aula 15: Elseif

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let num1 = 3;
-let num2 = 5;
+let num2 = 2;
 
 if(num1 > num2){
 	console.log(`${num1} é maior que ${num2}`);
+} else if(num1 === num2) {
+	console.log(`${num1} é igual a ${num2}`);
 } else {
 	console.log(`${num1} é menor que ${num2}`);
 }
