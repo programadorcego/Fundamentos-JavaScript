ddfd420bb39b385e922879c970574958b487c6a5
Programador Cego <programadorcego@gmail.com>
2023-03-02 07:38:52 -0300

    Aula 14: Else

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let num1 = 5;
-let num2 = 3;
+let num1 = 3;
+let num2 = 5;
 
 if(num1 > num2){
 	console.log(`${num1} é maior que ${num2}`);
+} else {
+	console.log(`${num1} é menor que ${num2}`);
 }
