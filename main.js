23a767772feae1d75bb752389a641d2999cd8edf
Programador Cego <programadorcego@gmail.com>
2023-03-06 22:33:15 -0300

    Aula 17: Operador Ternário

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-let fruta = "manga";
-
-switch(fruta) {
-	case "banana" :
-		console.log("Banana custa R$ 1,00 o quilo");
-	break;
-	
-	case "maçã" :
-		console.log("Maçã custa R$ 2,00 o quilo");
-	break;
-	
-	case "uva" :
-		console.log("Uva custa R$ 3,00 o quilo");
-	break;
-	
-	default :
-		console.log(`Desculpe. não há ${fruta} disponível no momento!`);
-	break;
-}
+let isBirthday = false;
+let gretting = (isBirthday) ? "Olá, Willian. Feliz aniversário" : "Olá, Willian";
+console.log(gretting);
