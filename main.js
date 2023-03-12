bf9c1167957b5005d4304545fe0de27070babb04
Programador Cego <programadorcego@gmail.com>
2023-03-12 17:59:30 -0300

    Aula 21: Declarações break e continue

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-let count = 0;
+for(let i = 0; i <= 10; i++)
+{
+	/*if(i == 6)
+	{
+		break;
+	}*/
 
-do {
-	console.log(count);
-	count++;
-} while(count < 10);
+	if(i % 2 == 0)
+	{
+		continue;
+	}
+	
+	console.log(i);
+}
